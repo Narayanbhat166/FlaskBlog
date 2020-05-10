e92d3f788a1e5e13f63454e5167065ddfc563ef8
Narayan Bhat <bhatj1964@gmail.com>
2020-05-10 15:46:32 +0530

    Everything Working fine untill now

---
+++ flaskblog/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bd6e3d4510ac627f0cd389ef6f74cfd5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_ID')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASS')
+mail = Mail(app)
+
+
+
+from flaskblog import routes
