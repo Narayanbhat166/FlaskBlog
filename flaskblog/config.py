636b0d995e11b61011a58a53682e9fce45bd0998
Narayan Bhat <bhatj1964@gmail.com>
2020-06-01 18:58:57 +0530

    Added tinymce as WYWIWYG

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT= 465
     MAIL_USE_SSL = True
